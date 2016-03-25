4ed63bfcc6984e5eb4c29a4cfa91f68327a90008
ngockhiem27 <ngockhiem27@gmail.com>
2016-03-25 09:50:08 +0700

    done

---
+++ app/src/androidTest/java/com/example/ngockhiem27/helloopencv/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.ngockhiem27.helloopencv;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/ngockhiem27/helloopencv/MainActivity.java
@@ -0,0 +1,252 @@
+package com.example.ngockhiem27.helloopencv;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private Mat originalMat;
+    private Bitmap currentBitmap;
+    private Bitmap originalBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.e(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d(this.getClass().getSimpleName(), "  OpenCVLoader.initDebug(), working.");
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_gallery) {
+            Intent intent_pick = new Intent(Intent.ACTION_PICK, Uri.parse("content://media/internal/images/media"));
+            startActivityForResult(intent_pick, 0);
+        }
+        if (id == R.id.DoG) {
+            if (originalBitmap != null) {
+                DifferenceOfGaussian();
+            }
+
+        }
+        if (id == R.id.CannyEdge) {
+            if (originalBitmap != null) {
+                Canny();
+            }
+        }
+        if (id == R.id.Sobel) {
+            if (originalBitmap != null) {
+                Sobel();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImg = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImg, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            // speed up loading IMG
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 2;
+            Bitmap temp = BitmapFactory.decodeFile(picturePath, options);
+
+            //Get orientation information
+            int orientation = 0;
+            try {
+                ExifInterface imgParams = new ExifInterface(picturePath);
+                orientation = imgParams.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //Rotating img to get correct orientation
+            Matrix rotate90 = new Matrix();
+            rotate90.postRotate(orientation);
+            originalBitmap = rotateBitmap(temp, orientation);
+
+            //Convert bitmap to Mat
+            Bitmap tempBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
+            originalMat = new Mat(tempBitmap.getHeight(), tempBitmap.getWidth(), CvType.CV_8U);
+            Utils.bitmapToMat(tempBitmap, originalMat);
+            currentBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, false);
+            loadImageToImageView();
+        }
+    }
+
+    private void loadImageToImageView() {
+        ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setImageBitmap(currentBitmap);
+    }
+
+    //Difference of Gaussian
+    public void DifferenceOfGaussian() {
+        Mat grayMat = new Mat();
+        Mat blur1 = new Mat();
+        Mat blur2 = new Mat();
+
+        //Converting the image to grayscale
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        Imgproc.GaussianBlur(grayMat, blur1, new Size(15, 15), 5);
+        Imgproc.GaussianBlur(grayMat, blur2, new Size(21, 21), 5);
+
+        //Subtracting the two blurred images
+        Mat DoG = new Mat();
+        Core.absdiff(blur1, blur2, DoG);
+
+        //Inverse Binary Thresholding
+        Core.multiply(DoG, new Scalar(100), DoG);
+        Imgproc.threshold(DoG, DoG, 50, 255, Imgproc.THRESH_BINARY_INV);
+
+        //Converting Mat back to Bitmap
+        Utils.matToBitmap(DoG, currentBitmap);
+        loadImageToImageView();
+    }
+
+    //Canny Edge Detection
+    void Canny() {
+        Mat grayMat = new Mat();
+        Mat cannyEdges = new Mat();
+        //Converting the image to grayscale
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        Imgproc.Canny(grayMat, cannyEdges, 10, 100);
+
+        //Converting Mat back to Bitmap
+        Utils.matToBitmap(cannyEdges, currentBitmap);
+        loadImageToImageView();
+    }
+
+    //Sobel Operator
+    void Sobel() {
+        Mat grayMat = new Mat();
+        Mat sobel = new Mat(); //Mat to store the final result
+
+        //Matrices to store gradient and absolute gradient respectively
+        Mat grad_x = new Mat();
+        Mat abs_grad_x = new Mat();
+
+        Mat grad_y = new Mat();
+        Mat abs_grad_y = new Mat();
+
+        //Converting the image to grayscale
+        Imgproc.cvtColor(originalMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+
+        //Calculating gradient in horizontal direction
+        Imgproc.Sobel(grayMat, grad_x, CvType.CV_16S, 1, 0, 3, 1, 0);
+
+        //Calculating gradient in vertical direction
+        Imgproc.Sobel(grayMat, grad_y, CvType.CV_16S, 0, 1, 3, 1, 0);
+
+        //Calculating absolute value of gradients in both the direction
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
+
+        //Calculating the resultant gradient
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 1, sobel);
+
+        //Converting Mat back to Bitmap
+        Utils.matToBitmap(sobel, currentBitmap);
+        loadImageToImageView();
+    }
+
+    //Function to rotate bitmap according to image parameters
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
